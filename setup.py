e2b850becbaa96a695e761d70684c7a5bbf88e5f
GrandMoff100 <minecraftcrusher100@gmail.com>
2020-08-20 10:10:46 -0500

    Added Setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Discordgame',
+    version='0.0.0',
+    packages=['discordgame'],
+    url='https://github.com/GrandMoff100/Discordgame',
+    license='GNU License',
+    author='Pixymon',
+    author_email='nlarseb23.student@gmail.com',
+    description='A Python Discord Library for developing games in Discord Servers.'
+)
