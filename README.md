29b2987adde1533b55ef2b8a67282d05894414e8
GrandMoff100 <minecraftcrusher100@gmail.com>
2020-08-24 20:53:54 -0500

    setup.py changes

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # DiscordGame
-A Python Framework for Making Text and Reaction Button Based Games in Discord.
+*DiscordGame is a Python Framework for Making Text and Reaction Button Based Games in Discord.*
+
+## Getting Started
+### Installation
+```shell script
+$ pip install discordgame
+```
+Or clone the repo
+
+```shell script
+$ git clone https://github.com/GrandMoff100/DiscordGame
+```
+
+and run
--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 import requests
 import datetime
 import subprocess
+import sys
 import os
 
 __project_name__ = 'DiscordGame'
@@ -33,18 +34,19 @@ setup(
     long_description_content_type='text/markdown'
 )
 
-user_env = os.environ.copy()
-directories = ['dist/', 'build/', __project_name__ + '.egg-info/']
-twine = subprocess.Popen(['twine', 'upload', 'dist/*'], env=user_env)
+if sys.argv[1] in ['sdist', 'bdist_wheel']:
+    user_env = os.environ.copy()
+    directories = ['dist/', 'build/', __project_name__ + '.egg-info/']
+    twine = subprocess.Popen(['twine', 'upload', 'dist/*'], env=user_env)
 
-def recursively_delete_directory(directory: str):
-    for file in os.listdir(directory):
-        if os.path.isdir(os.path.join(directory, file)):
-            recursively_delete_directory(os.path.join(directory, file))
-        elif os.path.isfile(os.path.join(directory, file)):
-            os.remove(os.path.join(directory, file))
-    os.rmdir(directory)
+    def recursively_delete_directory(directory: str):
+        for file in os.listdir(directory):
+            if os.path.isdir(os.path.join(directory, file)):
+                recursively_delete_directory(os.path.join(directory, file))
+            elif os.path.isfile(os.path.join(directory, file)):
+                os.remove(os.path.join(directory, file))
+        os.rmdir(directory)
 
-print(twine.communicate())
-for directory in directories:
-    recursively_delete_directory(directory)
+    print(twine.communicate())
+    for directory in directories:
+        recursively_delete_directory(directory)
